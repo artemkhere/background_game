f51a217c5099a4b4c90cd394cc3e46cbbd9992f2
Artem Kuznetsov <artem.k.here@gmail.com>
2020-04-12 18:54:01 -0700

    Added clickable area

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from "react";
 import socketIOClient from "socket.io-client";
 
+import ClickableArea from "./components/clickableArea";
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
       response: false,
-      endpoint: "http://127.0.0.1:6969"
+      endpoint: "http://127.0.0.1:6969",
+      socket: false // should be ported out into global state so anything can grab it
     };
   }
 
   componentDidMount() {
     const { endpoint } = this.state;
     const socket = socketIOClient(endpoint);
+    this.setState({ socket });
     socket.on("FromAPI", (data) => this.setState({ response: data }));
   }
 
@@ -26,6 +30,8 @@ class App extends Component {
                 The temperature in Florence is: {response} °F
               </p>
               : <p>Loading...</p>}
+
+          <ClickableArea socket={this.state.socket} />
         </div>
     );
   }
