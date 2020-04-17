ed332d24c1585bb3859d753432b8f74d17a80c26
Artem Kuznetsov <artem.k.here@gmail.com>
2020-04-17 13:53:47 -0700

    Socket.io added

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import http from 'http';
 import socketIO from 'socket.io';
-import axios from 'axios';
 
 import index from './routes/index';
 
@@ -13,13 +12,8 @@ const server = http.createServer(app);
 
 const io = socketIO(server);
 
-// app.get('/', (req, res) => res.send('Hello World!'));
-// app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
-
 io.on("connection", (socket) => {
   console.log("New client connected");
-  // console.log(socket);
-  // setInterval(() => getApiAndEmit(socket), 10000);
   socket.emit("FromAPI", "You connected, brah.");
 
   socket.on('areaClicked', (data) => {
